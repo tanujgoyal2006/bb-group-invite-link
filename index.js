140af660f333c188237c84c284ae10dcf6eabae5
tanujgoyal2006 <57218823+tanujgoyal2006@users.noreply.github.com>
2022-05-23 23:30:15 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,18 +1,11 @@
 import React from "react";
 import {View, Text} from "react-native";
-import MyCustomScreen from "./components/MyCustomScreen";
 export const applyCustomCode = externalCodeSetup => {
-
- externalCodeSetup.navigationApi.addNavigationRoute(
-   "book",
-   "BookScreen",
-   MyCustomScreen,
-   "All"
- );
- externalCodeSetup.navigationApi.addNavigationRoute(
-   "book",
-   "BookScreen",
-   MyCustomScreen,
-   "Main"
- );
-}
+    // call custom code api here
+    // externalCodeSetup.configApi.setAppSwitchEnabled(true);
+    externalCodeSetup.navigationApi.replaceScreenComponent("GroupScreen", () => (
+        <View style={{flex: 1, alignItems: "center", justifyContent: "center"}}>
+            <Text>This is my signup screen.</Text>
+        </View>
+    ));
+};
